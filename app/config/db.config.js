d21205c9a068f544e6058997d3f0b21955d99c70
Redrock912 <leset912@gmail.com>
2019-11-07 16:52:21 +0900

    changed config for heroku

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,13 +1,13 @@
-// module.exports = {
-//   HOST: "us-cdbr-iron-east-05.cleardb.net",
-//   USER: "bf30a3c05fa183",
-//   PASSWORD: "8086d028",
-//   DB: "heroku_858b2c7635f5aef"
-// };
-
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "",
-  DB: "gb"
+  HOST: "us-cdbr-iron-east-05.cleardb.net",
+  USER: "bf30a3c05fa183",
+  PASSWORD: "8086d028",
+  DB: "heroku_858b2c7635f5aef"
 };
+
+// module.exports = {
+//   HOST: "localhost",
+//   USER: "root",
+//   PASSWORD: "",
+//   DB: "gb"
+// };
